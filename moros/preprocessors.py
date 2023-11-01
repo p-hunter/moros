a00073a9a488c241d63d94740d3a050a885f6219
Phil Hunter <71961012+p-hunter@users.noreply.github.com>
2023-11-01 15:18:07 +0000

    expansion of label lumping

--- moros/preprocessors.py
+++ moros/preprocessors.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 
 class Lumper(BaseEstimator, TransformerMixin):
-    def __init__(self, thres=0.01, other="other"):
+    def __init__(self, thres=0.01, other = "other", enc = OneHotEncoder()):
         self.thres = thres
-        self.other = other    
+        self.other = other
+        self.encoder = enc
 
     def fit(self, X, y=None):
         return(self)
@@ -14,5 +15,46 @@ class Lumper(BaseEstimator, TransformerMixin):
         vc = X.value_counts()
         crit = (vc/vc.sum()).lt(self.thres)
         X = np.array(np.where(X.isin(vc[crit].index), self.other, X))
-        X = OneHotEncoder().fit_transform(X)
-        return(X)
+        X = self.encoder.fit_transform(X)
+        return X
+
+class OneHotLumper(BaseEstimator, TransformerMixin):
+    def __init__(self, thres=0.01, other="other", enc=OneHotEncoder()):
+        """
+
+        :type enc: encoder (just one-hot)
+        """
+        self.thres = thres
+        self.other = other
+        self.encoder = enc
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        vc = X.value_counts()
+        crit = (vc / vc.sum()).lt(self.thres)
+        X = np.array(np.where(X.isin(vc[crit].index), self.other, X))
+        X = self.encoder.fit_transform(X)
+        return X
+
+
+class OrdinalLumper(BaseEstimator, TransformerMixin):
+    def __init__(self, thres=0.01, other="other"):
+        self.thres = thres
+        self.other = other
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        vc = X.value_counts()
+        crit = (vc / vc.sum()).lt(self.thres)
+        X = np.array(np.where(X.isin(vc[crit].index), self.other, X))
+        vc2 = pd.Series(X).value_counts()
+        c_order = vc2.index.values.tolist()
+        if self.other in c_order:
+            c_order.sort(key=self.other.__eq__)
+        c_order = [np.array(c_order)]
+        X = OrdinalEncoder(categories=c_order).fit_transform(X.reshape(-1, 1))
+        return X
